80efea197bbf3d2ff175977b5257c520a82fc7ca
merveucer <merveucer.txt@gmail.com>
2021-04-08 19:02:19 +0300

    Add User, Customer, Rental

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,4 +1,5 @@
-﻿using Business.Concrete;
+﻿using Business.Abstract;
+using Business.Concrete;
 using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
@@ -10,11 +11,147 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            CarTest();
+            //CarTest();
 
-            BrandTest();
+            //BrandTest();
 
-            ColorTest();
+            //ColorTest();
+
+            //CustomerTest();
+
+            //UserTest();
+
+            RentalTest();
+        }
+
+        private static void RentalTest()
+        {
+            Console.WriteLine("--------- RENTAL TEST ---------");
+            IRentalService rentalManager = new RentalManager(new EfRentalDal());
+
+            Console.WriteLine("--------- GetAll ---------");
+            foreach (var rental in rentalManager.GetAll().Data)
+            {
+                Console.WriteLine(rental.RentalId + " " + rental.ReturnDate);
+            }
+
+            Console.WriteLine("--------- GetById ---------");
+            Console.WriteLine(rentalManager.GetById(1).Data.RentalId + " " + rentalManager.GetById(1).Data.ReturnDate);
+
+            Console.WriteLine("--------- Add ---------");
+            var resultAdd = rentalManager.Add(new Rental { RentalId = 6, CarId = 1, CustomerId = 1, RentDate = DateTime.Now, ReturnDate = null });
+            Console.WriteLine(resultAdd.Message);
+            foreach (var rental in rentalManager.GetAll().Data)
+            {
+                Console.WriteLine(rental.RentalId + " " + rental.ReturnDate);
+            }
+
+            Console.WriteLine("--------- Cars That Can Be Rented ---------");
+            CarManager carManager = new CarManager(new EfCarDal());
+            foreach (var rental in rentalManager.GetAll().Data)
+            {
+                if (rental.ReturnDate != null)
+                {
+                    Console.WriteLine(carManager.GetById(rental.CarId).Data.CarName);
+                }
+            }
+
+            Console.WriteLine("--------- Rental Process Fail ---------");
+            Console.WriteLine(rentalManager.Add(new Rental { RentalId = 7, CarId = 1, CustomerId = 1, RentDate = DateTime.Now, ReturnDate = null }).Message);
+
+            Console.WriteLine("--------- Update ---------");
+            var resultUpdate = rentalManager.Update(new Rental { RentalId = 6, CarId = 1, CustomerId = 1, RentDate = DateTime.Now, ReturnDate = DateTime.Now });
+            Console.WriteLine(resultUpdate.Message);
+            foreach (var rental in rentalManager.GetAll().Data)
+            {
+                Console.WriteLine(rental.RentalId + " " + rental.ReturnDate);
+            }
+
+            Console.WriteLine("--------- Delete ---------");
+            var resultDelete = rentalManager.Delete(new Rental { RentalId = 6 });
+            Console.WriteLine(resultDelete.Message);
+            foreach (var rental in rentalManager.GetAll().Data)
+            {
+                Console.WriteLine(rental.RentalId + " " + rental.ReturnDate);
+            }
+        }
+
+        private static void UserTest()
+        {
+            Console.WriteLine("--------- USER TEST ---------");
+            UserManager userManager = new UserManager(new EfUserDal());
+
+            Console.WriteLine("--------- GetAll ---------");
+            foreach (var user in userManager.GetAll().Data)
+            {
+                Console.WriteLine(user.FirstName + " " + user.LastName);
+            }
+
+            Console.WriteLine("--------- GetById ---------");
+            Console.WriteLine(userManager.GetById(1).Data.FirstName + " " + userManager.GetById(1).Data.LastName);
+
+            Console.WriteLine("--------- Add ---------");
+            var resultAdd = userManager.Add(new User { UserId = 6, FirstName = "Ffff", LastName = "Ffff", Email = "f@gmail.com", Password = "f" });
+            Console.WriteLine(resultAdd.Message);
+            foreach (var user in userManager.GetAll().Data)
+            {
+                Console.WriteLine(user.FirstName + " " + user.LastName);
+            }
+
+            Console.WriteLine("--------- Update ---------");
+            var resultUpdate = userManager.Update(new User { UserId = 6, FirstName = "Fffffff", LastName = "Fffffff", Email = "f@gmail.com", Password = "f" });
+            Console.WriteLine(resultUpdate.Message);
+            foreach (var user in userManager.GetAll().Data)
+            {
+                Console.WriteLine(user.FirstName + " " + user.LastName);
+            }
+
+            Console.WriteLine("--------- Delete ---------");
+            var resultDelete = userManager.Delete(new User { UserId = 6 });
+            Console.WriteLine(resultDelete.Message);
+            foreach (var user in userManager.GetAll().Data)
+            {
+                Console.WriteLine(user.FirstName + " " + user.LastName);
+            }
+        }
+
+        private static void CustomerTest()
+        {
+            Console.WriteLine("--------- CUSTOMER TEST ---------");
+            CustomerManager customerManager = new CustomerManager(new EfCustomerDal());
+
+            Console.WriteLine("--------- GetAll ---------");
+            foreach (var customer in customerManager.GetAll().Data)
+            {
+                Console.WriteLine(customer.CustomerId + " " + customer.CompanyName);
+            }
+
+            Console.WriteLine("--------- GetById ---------");
+            Console.WriteLine(customerManager.GetById(1).Data.CustomerId + " " + customerManager.GetById(1).Data.CompanyName);
+
+            Console.WriteLine("--------- Add ---------");
+            var resultAdd = customerManager.Add(new Customer { CustomerId = 6, UserId = 1, CompanyName = "F" });
+            Console.WriteLine(resultAdd.Message);
+            foreach (var customer in customerManager.GetAll().Data)
+            {
+                Console.WriteLine(customer.CustomerId + " " + customer.CompanyName);
+            }
+
+            Console.WriteLine("--------- Update ---------");
+            var resultUpdate = customerManager.Update(new Customer { CustomerId = 6, UserId = 1, CompanyName = "FFF" });
+            Console.WriteLine(resultUpdate.Message);
+            foreach (var customer in customerManager.GetAll().Data)
+            {
+                Console.WriteLine(customer.CustomerId + " " + customer.CompanyName);
+            }
+
+            Console.WriteLine("--------- Delete ---------");
+            var resultDelete = customerManager.Delete(new Customer { CustomerId = 6 });
+            Console.WriteLine(resultDelete.Message);
+            foreach (var customer in customerManager.GetAll().Data)
+            {
+                Console.WriteLine(customer.CustomerId + " " + customer.CompanyName);
+            }
         }
 
         private static void ColorTest()
@@ -103,22 +240,22 @@ namespace ConsoleUI
             Console.WriteLine("--------- GetAll ---------");
             foreach (var car in carManager.GetAll().Data)
             {
-                Console.WriteLine("Car Name: " + car.CarName);
+                Console.WriteLine(car.CarName);
             }
 
             Console.WriteLine("--------- GetById ---------");
-            Console.WriteLine("Car Name: " + carManager.GetById(1).Data.CarName);
+            Console.WriteLine(carManager.GetById(1).Data.CarName);
 
             Console.WriteLine("--------- GetCarsByBrandId ---------");
             foreach (var car in carManager.GetCarsByBrandId(1).Data)
             {
-                Console.WriteLine("Car Name: " + car.CarName);
+                Console.WriteLine(car.CarName);
             }
 
             Console.WriteLine("--------- GetCarsByColorId ---------");
             foreach (var car in carManager.GetCarsByColorId(1).Data)
             {
-                Console.WriteLine("Car Name: " + car.CarName);
+                Console.WriteLine(car.CarName);
             }
 
             Console.WriteLine("--------- GetCarsByDailyPrice ---------");
@@ -127,7 +264,7 @@ namespace ConsoleUI
             {
                 foreach (var car in result.Data)
                 {
-                    Console.WriteLine("Car Name: " + car.CarName);
+                    Console.WriteLine(car.CarName);
                 }
             }
             else
@@ -138,23 +275,23 @@ namespace ConsoleUI
             Console.WriteLine("--------- GetCarDetails ---------");
             foreach (var car in carManager.GetCarDetails().Data)
             {
-                Console.WriteLine("Car Name: " + car.CarName + " Brand Name: " + car.BrandName + " Color Name:" + car.ColorName + " Daily Price: " + car.DailyPrice);
+                Console.WriteLine("Car Name: " + car.CarName + " - Brand Name: " + car.BrandName + " - Color Name:" + car.ColorName + " - Daily Price: " + car.DailyPrice);
             }
 
             Console.WriteLine("--------- Add ---------");
-            var resultAdd = carManager.Add(new Car { CarId = 6, BrandId = 1, ColorId = 1, DailyPrice = 600, Description = "fff", ModelYear = 2016, CarName = "FF" });
+            var resultAdd = carManager.Add(new Car { CarId = 6, BrandId = 1, ColorId = 1, DailyPrice = 600, Description = "x", ModelYear = 2016, CarName = "Araba66" });
             Console.WriteLine(resultAdd.Message);
             foreach (var car in carManager.GetAll().Data)
             {
-                Console.WriteLine("Car Name: " + car.CarName);
+                Console.WriteLine(car.CarName);
             }
 
             Console.WriteLine("--------- Update ---------");
-            var resultUpdate =carManager.Update(new Car { CarId = 6, BrandId = 1, ColorId = 1, DailyPrice = 600, Description = "fff", ModelYear = 2016, CarName = "FFFFF" });
+            var resultUpdate =carManager.Update(new Car { CarId = 6, BrandId = 1, ColorId = 1, DailyPrice = 600, Description = "x", ModelYear = 2016, CarName = "Araba6" });
             Console.WriteLine(resultUpdate.Message);
             foreach (var car in carManager.GetAll().Data)
             {
-                Console.WriteLine("Car Name: " + car.CarName);
+                Console.WriteLine(car.CarName);
             }
 
             Console.WriteLine("--------- Delete ---------");
@@ -162,11 +299,11 @@ namespace ConsoleUI
             Console.WriteLine(resultDelete.Message);
             foreach (var car in carManager.GetAll().Data)
             {
-                Console.WriteLine("Car Name: " + car.CarName);
+                Console.WriteLine(car.CarName);
             }
 
             Console.WriteLine("--------- Adding Process Fail ---------");
-            Console.WriteLine(carManager.Add(new Car { CarId = 6, BrandId = 1, ColorId = 1, DailyPrice = 600, Description = "fff", ModelYear = 2016, CarName = "F" }).Message);
+            Console.WriteLine(carManager.Add(new Car { CarId = 6, BrandId = 1, ColorId = 1, DailyPrice = 600, Description = "x", ModelYear = 2016, CarName = "A" }).Message);
         }
     }
 }
